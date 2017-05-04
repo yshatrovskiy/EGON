38381686d21baf0684a3dd54cff71bd4bdb29806
yshatrovskiy <yshatrovskiy@gmail.com>
2017-05-04 13:28:51 -0400

    new

--- app/src/main/java/com/eg/yevgeniy/egontrial/Activities/ManageEnergy/AptAdapter.java
+++ app/src/main/java/com/eg/yevgeniy/egontrial/Activities/ManageEnergy/AptAdapter.java
@@ -19,10 +19,11 @@ public class AptAdapter extends RecyclerView.Adapter<AptViewHolder> {
 
     public List<unitEnergy> unitList = new ArrayList<>();
 
+
     public AptAdapter(){
-        unitList.add(new unitEnergy(7, 45));
+        unitList.add(new unitEnergy(7, 5));
         unitList.add(new unitEnergy(15, 7));
-        unitList.add(new unitEnergy(7, 45));
+        unitList.add(new unitEnergy(7, 5));
         unitList.add(new unitEnergy(15, 7));
     }
 
@@ -54,14 +55,42 @@ public class AptAdapter extends RecyclerView.Adapter<AptViewHolder> {
 
     }
 
+    public void makePurchase(int x){
+        int amount = x;
+        int target = getTotalValue();
+
+        if(amount <= target){
+            for(int i = 0; amount > 0;i++){
+
+                if(unitList.get(i).getEnergyAmount() == amount){
+                    amount = 0;
+                    removeUnit(i);
+                    i--;
+                }else if(unitList.get(i).getEnergyAmount() > amount){
+                    unitList.get(i).setEnergyAmount(unitList.get(i).getEnergyAmount() - amount);
+                    amount = 0;
+                }else if (amount > unitList.get(i).getEnergyAmount()){
+                    amount = amount - unitList.get(i).getEnergyAmount();
+                    removeUnit(i);
+                    i--;
+                }
+            }
+
+        }
+
+
+
+    }
+
     public void addUnit(int x, int y){
         unitList.add(new unitEnergy(x, y));
     }
 
     public int getTotalValue(){
+
         int total = 0;
 
-        for(int i = 0; i <= unitList.size(); i++){
+        for(int i = 0; i <= unitList.size()-1; i++){
             total += unitList.get(i).getEnergyAmount();
         }
         return total;
--- app/src/main/java/com/eg/yevgeniy/egontrial/Activities/ManageEnergy/ManageFragment.java
+++ app/src/main/java/com/eg/yevgeniy/egontrial/Activities/ManageEnergy/ManageFragment.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.eg.yevgeniy.egontrial.Activities.Dashboard.CustomerAdapter;
@@ -24,11 +26,26 @@ public class ManageFragment extends android.support.v4.app.Fragment {
 
     private AptAdapter adapter;
 
-    TextView availableKwHBox;
-    TextView howMuchBox;
-    TextView totalToSellorBuy;
-    Button plus;
-    Button minus;
+
+
+    int counter = 0;
+    int available = 0;
+
+    TextView tViewTotal;
+    TextView tViewCounter;
+    TextView tViewAvailable;
+
+    Button bPlus;
+    Button bMinus;
+    Button bBuy;
+    Button bSell;
+
+    Button agree;
+    Button disagree;
+
+    Button bBuyTotal;
+
+    PopupWindow pw;
 
 
 
@@ -54,55 +71,166 @@ public class ManageFragment extends android.support.v4.app.Fragment {
 
         getActivity().setRequestedOrientation(
                 ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-
-
         View v = inflater.inflate(R.layout.fragment_manage, container, false);
 
-        Button button = (Button)v.findViewById(R.id.button2);
-        button.setOnClickListener(test);
-
-                RecyclerView recyclerView = (RecyclerView)v.findViewById(R.id.apt_recycler_stations);
+        RecyclerView recyclerView = (RecyclerView)v.findViewById(R.id.apt_recycler_stations);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         adapter = new AptAdapter();
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
 
+        tViewTotal = (TextView)v.findViewById(R.id.textViewTotal);
+        tViewCounter = (TextView)v.findViewById(R.id.textViewCounter);
+        tViewAvailable = (TextView)v.findViewById(R.id.textViewEnergy);
+
+        setAvailable();
+        setPrice();
+
+        bPlus = (Button)v.findViewById(R.id.buttonPlus);
+        bPlus.setOnClickListener(test);
+
+        bMinus = (Button)v.findViewById(R.id.buttonMinus);
+        bMinus.setOnClickListener(test);
+
+        bBuy = (Button)v.findViewById(R.id.buttonBuy);
+        bBuy.setOnClickListener(choice);
+
+        bSell = (Button)v.findViewById(R.id.buttonSell);
+        bSell.setOnClickListener(choice);
+
+
+        bBuyTotal = (Button)v.findViewById(R.id.buttonBuyTotal);
+        bBuyTotal.setText("Buy / Sell");
+
+
+        View popupView = LayoutInflater.from(getActivity()).inflate(R.layout.popup_window, null);
+        agree = (Button)popupView.findViewById(R.id.agree);
+        disagree = (Button)popupView.findViewById(R.id.disagree);
+        pw = new PopupWindow(popupView, 300, 300, true);
 
 
         return v;
     }
 
+    View.OnClickListener buy = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            pw.showAtLocation(getView(), Gravity.CENTER, 0, 0);
+            disagree.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    pw.dismiss();
+                }
+            });
+            agree.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    adapter.makePurchase(counter);
+                    adapter.notifyDataSetChanged();
+                    available = adapter.getTotalValue();
+                    setCounter(0);
+                    setAvailable();
+                    setPrice();
+                    pw.dismiss();
+                }
+            });
+        }
+    };
+
+    View.OnClickListener sell = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            pw.showAtLocation(getView(), Gravity.CENTER, 0, 0);
+            disagree.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    pw.dismiss();
+                }
+            });
+            agree.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    adapter.addUnit(counter, counter);
+                    adapter.notifyDataSetChanged();
+                    available =adapter.getTotalValue();
+                    setCounter(0);
+                    setAvailable();
+                    setPrice();
+                    pw.dismiss();
+                }
+            });
+        }
+    };
 
 
-    public String determinePrice(int kwh){
-
-        String finalPrice = "";
-
-
-
-        return finalPrice;
+    public void plusOne(){
+        if(available > counter){
+            counter++;
+            tViewCounter.setText(counter + " kWh");
+        }
     }
 
-    public int determineAvailablekWh(){
 
-        return adapter.getTotalValue();
+    public int getCounter() {
+        return counter;
     }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+        tViewCounter.setText(counter + " kWh");
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public void setAvailable() {
+        available = adapter.getTotalValue();
+        tViewAvailable.setText(available + " kWh");
+    }
+
+    public void minusOne(){
+        if(0 < counter){
+            counter--;
+            tViewCounter.setText(counter + " kWh");
+        }
+    }
+
+    public void setPrice(){
+        tViewTotal.setText((counter * .18) + " Euros");
+    }
+
+    View.OnClickListener choice = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()){
+                case R.id.buttonBuy:
+                    bBuyTotal.setBackgroundResource(R.drawable.rectangle_green);
+                    bBuyTotal.setText("Buy");
+                    bBuyTotal.setOnClickListener(buy);
+                    break;
+                case R.id.buttonSell:
+                    bBuyTotal.setBackgroundResource(R.drawable.rectangle_orange);
+                    bBuyTotal.setText("Sell");
+                    bBuyTotal.setOnClickListener(sell);
+                    break;
+            }
+        }
+    };
 
     View.OnClickListener test = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch(v.getId()){
-                case R.id.button2:
-                    adapter.removeUnit(0);
-                    adapter.notifyDataSetChanged();
+                case R.id.buttonMinus:
+                    minusOne();
+                    setPrice();
+                    break;
+                case R.id.buttonPlus:
+                    plusOne();
+                    setPrice();
                     break;
             }
-
-
-
         }
     };
-
-
 }
